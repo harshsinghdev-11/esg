5635eccffc7a6d82556baab03027b59b869cc950
kushal <kushalpalbishnoi@gmail.com>
2026-07-12 09:43:35 +0530

    backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express, { type Request, type Response } from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
